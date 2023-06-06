19febe26d9b2e16629c3695c186fae6ea7020265
innocent259 <ndahimanainnocent842@gmail.com>
2023-06-06 17:35:21 +0200

    Resolve linters errors

--- modules/bookUtilits.js
+++ modules/bookUtilits.js
@@ -42,13 +42,13 @@ export const addBook = (event, booksCollection, displayBooks, saveBooksToStorage
   booksCollection.push(newBook);
   titleInput.value = '';
   authorInput.value = '';
-  displayBooks(booksCollection, removeBook);
+  displayBooks();
   saveBooksToStorage();
 };
 
 export const removeBook = (index, booksCollection, displayBooks, saveBooksToStorage) => {
   booksCollection.splice(index, 1);
-  displayBooks(booksCollection, removeBook);
+  displayBooks();
   saveBooksToStorage();
 };
 
@@ -56,41 +56,16 @@ export const showAddSection = (addSection, listSection, contactSection) => {
   addSection.style.display = 'block';
   listSection.style.display = 'none';
   contactSection.style.display = 'none';
-
-  // Store active section in localStorage
-  localStorage.setItem('activeSection', 'addSection');
 };
 
 export const showListSection = (addSection, listSection, contactSection) => {
   addSection.style.display = 'none';
   listSection.style.display = 'block';
   contactSection.style.display = 'none';
-
-  // Store active section in localStorage
-  localStorage.setItem('activeSection', 'listSection');
 };
 
 export const showContactSection = (addSection, listSection, contactSection) => {
   addSection.style.display = 'none';
   listSection.style.display = 'none';
   contactSection.style.display = 'block';
-
-  // Store active section in localStorage
-  localStorage.setItem('activeSection', 'contactSection');
 };
-
-window.addEventListener('DOMContentLoaded', () => {
-  const addSection = document.querySelector('#addSection');
-  const listSection = document.querySelector('#listSection');
-  const contactSection = document.querySelector('#contactSection');
-
-  const activeSection = localStorage.getItem('activeSection');
-
-  if (activeSection === 'addSection') {
-    showAddSection(addSection, listSection, contactSection);
-  } else if (activeSection === 'contactSection') {
-    showContactSection(addSection, listSection, contactSection);
-  } else {
-    showListSection(addSection, listSection, contactSection);
-  }
-});
